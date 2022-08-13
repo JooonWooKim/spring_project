c7b496a4b2e5763c92ed01f31cc76581b8f63624
Joonwoo Kim <kkn8010@gmail.com>
2022-08-13 18:43:49 +0900

    docs: 전체 회원 정보 select 기능 추가, test: paging test

--- src/main/java/com/cos/new_project/test/DummyControllerTest.java
+++ src/main/java/com/cos/new_project/test/DummyControllerTest.java
@@ -1,9 +1,14 @@
 package com.cos.new_project.test;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +24,21 @@ public class DummyControllerTest {
 
 	@Autowired	//의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	@GetMapping("/dummy/users")
+	public List<User>list(){
+		return userRepository.findAll();
+	}
+	
+	//paging
+	//한 페이지당 2건의 데이터를 리턴받기
+	@GetMapping("/dummy/user")
+	public List<User>pageList(@PageableDefault(size=2,sort="id",direction=Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User>users = pagingUser.getContent();
+		return users;
+	}
 	
 	//{id} 주소로 파라미터를 전달 받을 수 있다.
 	@GetMapping("/dummy/user/{id}")
