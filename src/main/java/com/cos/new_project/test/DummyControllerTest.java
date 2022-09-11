1d25d82f0f4f2d22be67bab59427e3691718fb4d
Joonwoo Kim <kkn8010@gmail.com>
2022-09-11 18:27:56 +0900

    docs: 해당 id의 정보 삭제 기능 추가

--- src/main/java/com/cos/new_project/test/DummyControllerTest.java
+++ src/main/java/com/cos/new_project/test/DummyControllerTest.java
@@ -7,10 +7,12 @@ import java.util.function.Supplier;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +35,19 @@ public class DummyControllerTest {
 	public List<User>list(){
 		return userRepository.findAll();
 	}
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			//Exception도 가능하지만 비어있는 상태의 Exception을 잡기위해서
+			return "삭제에 실패하였습니다. 해당 id는 존재하지않습니다.";
+		}
+		
+		return "삭제되었습니다.id:" + id;
+	}
+	
 	
 	//save함수는 id를 전달하지 않으면 insert를 해주고
 	//id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
@@ -57,7 +72,7 @@ public class DummyControllerTest {
 //		userRepository.save(user);
 		
 		//더티 체킹
-		return null;
+		return user;
 	}
 	
 	//paging
