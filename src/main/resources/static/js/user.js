0129c655d09c34dfb6982699ac78bdc8ba1e9730
Joonwoo Kim <kkn8010@gmail.com>
2022-09-14 17:45:35 +0900

    bug: login이 안되는 bug 해결

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -40,8 +40,7 @@ let index = {
 		//alert('user의 save함수 호출됨');
 		let data = {
 			username: $("#username").val(),
-			password: $("#password").val(),
-			email: $("#email").val()
+			password: $("#password").val()
 		}
 		
 		//console.log(data);
@@ -50,7 +49,7 @@ let index = {
 		$.ajax({
 			//회원가입 수행 요청
 			type:"POST",
-			url: "/blog/api/login",
+			url: "/blog/api/user/login",
 			data: JSON.stringify(data),	//http body데이터
 			contentType: "application/json; charset=utf-8",	
 			dataType: "json"
@@ -58,7 +57,8 @@ let index = {
 			alert("로그인이 완료되었습니다.");
 			location.href = "/blog";
 		}).fail(function(error){
-			alert(JSON.stringify(error));
+			alert("로그인 실패");
+			//alert(JSON.stringify(error));
 		});
 		//ajax 통신을 이용해서 3개의 객체를 json으로 변경하여 insert요청
 	}	
