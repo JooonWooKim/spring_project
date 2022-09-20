b3adb5f7140d874318ca7657754e10c38f539652
Joonwoo Kim <kkn8010@gmail.com>
2022-09-20 17:02:35 +0900

    docs: 게시판 기능 구현 작성

--- src/main/java/com/cos/new_project/controller/BoardController.java
+++ src/main/java/com/cos/new_project/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.cos.new_project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cos.new_project.config.auth.PrincipalDetail;
@@ -17,7 +18,8 @@ public class BoardController {
 //	@AuthenticationPrincipal principalDetail principal
 
 	@GetMapping({"", "/"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("boards", boardService.글목록());
 		return "index";
 	}
 	
