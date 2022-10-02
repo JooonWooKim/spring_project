abe085b6e9eb70003c297314137d709db43aea7c
JoonwooKim <kkn8010@gmail.com>
2022-10-02 18:13:41 +0900

    update: 유효성 체크 오류로 인한 회원가입이 안되던 오류 수정

--- src/main/java/com/cos/new_project/model/User.java
+++ src/main/java/com/cos/new_project/model/User.java
@@ -41,7 +41,7 @@ public class User {
 	
 	@Column(nullable=false, length=100)
 	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+	//@Pattern(regexp="[a-zA-Z1-9]{6,12}", message = "비밀번호는 영어와 숫자로 포함해서 6~12자리 이내로 입력해주세요.")
 	private String password;
 	
 	@Column(nullable=false, length=50)
