e8f6812f233c4081368e3f2e6c6d69779992b765
JoonwooKim <kkn8010@gmail.com>
2022-10-11 15:04:53 +0900

    update: userController, UserService Spring AOP 기능 작성 구현

--- src/main/java/com/cos/new_project/aop/LoggingAspect.java
+++ src/main/java/com/cos/new_project/aop/LoggingAspect.java
@@ -11,17 +11,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
  
-    @Before("execution(* com.cos.new_project.service.*.get*(..))")
+    @Before("execution(* com.cos.new_project.service.*.회원*(..))")
     public void loggerBefore() {
-        System.out.println("get으로 시작하는 메서드가 시작됩니다.");
+        System.out.println("회원으로 시작하는 메서드가 시작됩니다.");
     }
  
-    @After("execution(* com.cos.new_project.service.*.get*(..))")
+    @After("execution(* com.cos.new_project.service.*.회원*(..))")
     public void loggerAfter() {
-        System.out.println("get으로 시작하는 메서드가 끝났습니다.");
+        System.out.println("회원으로 시작하는 메서드가 끝났습니다.");
     }
  
-    @Around("execution(* com.cos.new_project.controller.UserController.*(..))")
+    @Around("execution(* com.cos.new_project.controller.*.Controller.*(..))")
     public Object loggerAround(ProceedingJoinPoint pjp) throws Throwable {
         long beforeTimeMillis = System.currentTimeMillis();
         System.out.println("[UserController] 실행 시작 : "
