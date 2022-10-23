85d8ca650dd32f39498114fc2eeef5fe0e46c86d
JoonwooKim <kkn8010@gmail.com>
2022-10-23 19:51:40 +0900

    update: SecurityConfig 내용 수정

--- src/main/java/com/cos/new_project/config/SecurityConfig.java
+++ src/main/java/com/cos/new_project/config/SecurityConfig.java
@@ -5,28 +5,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.cos.new_project.config.auth.PrincipalDetailService;
+import com.cos.new_project.model.RoleType;
 
 //빈 등록: 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
 
 @Configuration 	//빈 등록(IoC관리)
 @EnableWebSecurity	//시큐리티 필터가 등록이 된다.
 @EnableGlobalMethodSecurity(prePostEnabled = true)	//특정 주소로 접근하면 권한 인증을 미리 체크 
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig {
 
 	@Autowired
 	private PrincipalDetailService princiaplDetailService;
 	
+//	@Bean
+//	@Override
+//	public AuthenticationManager authenticationManagerBean() throws Exception {
+//		return super.authenticationManagerBean();
+//	}
+	
 	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+	return authenticationConfiguration.getAuthenticationManager();
 	}
 
 	@Bean
@@ -38,14 +49,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	//시큐리티가 대신 로그인해주는데 password를 가로채는데 해당 password가 뭘로 해쉬가 되어서 회원가입이 되었는지
 	//알아야 같은 해쉬로 암호화해서 db에 있는 해쉬랑 비교를 할 수 있다.
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(princiaplDetailService).passwordEncoder(encodePWD());
-	}
+//	@Bean
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(princiaplDetailService).passwordEncoder(encodePWD());
+//	}
+	
+//	@Bean
+//	public InMemoryUserDetailsManager userDetailsService() {
+//		UserDetails user = User.withDefaultPasswordEncoder()
+//				.username("user")
+//				.password("password")
+//				.roles("USER")
+//				.build();
+//		return new InMemoryUserDetailsManager(user);
+//	}
+	
+//	@Bean
+//	public WebSecurityCustomizer webSecurityCustomizer(AuthenticationManagerBuilder auth) throws Exception {
+//		return (WebSecurityCustomizer) auth
+//				.userDetailsService(princiaplDetailService).passwordEncoder(encodePWD());
+//	}
 	
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+//	@Override
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf().disable()
 			.authorizeHttpRequests()
@@ -59,5 +87,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 				.loginProcessingUrl("/auth/loginProc")
 				.defaultSuccessUrl("/");
 				//스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인을 해준다.
+		return http.build();
 	}
 }
